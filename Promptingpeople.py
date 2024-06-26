9161aec3aaf4355d3f1a4e33c5d24bb878cbe1dd
Shweta Shrivastava <Shweta.Shrivastava@thomsonreuters.com>
2024-06-26 22:47:14 +0530

    examples on python

---
+++ Promptingpeople.py
@@ -0,0 +1,14 @@
+#Prompting people 
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you are {age} yearsq old, {height} tall and {weight} heavy.")
+
+print("What is your name?", end=' ' )
+
+name = input()
+print(f"Hello, {name}!")
+#This is a comment
+
+
